4f90f1390d031084dc9d694803dcf91a181299dd
rabbit99 <vvbest2012@gmail.com>
2025-07-18 23:38:11 +0800

    update

--- GameSystem/_shared/Lake/Lake.gd
+++ GameSystem/_shared/Lake/Lake.gd
@@ -8,7 +8,7 @@ func interact()-> void:
 	if _busy: return 
 	
 	_busy = true
+	_god_scene_manager.start_scene_requested.emit()
 	var weapon_arr: Array[Weapon] = await _god_scene_manager.start_event()
 	await _weapon_editor.start_event(weapon_arr[0], weapon_arr[1])
 	_busy = false
-	
