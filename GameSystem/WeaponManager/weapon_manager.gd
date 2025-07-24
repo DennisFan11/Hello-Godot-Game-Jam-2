00e22c25fdd7728f1c307d6aab28e197b2d17fa5
DennisFan(Mac <dennis515jd@gmail.com>
2025-07-24 20:36:24 +0800

    Weapon auto_update_polygon

--- GameSystem/WeaponManager/weapon_manager.gd
+++ GameSystem/WeaponManager/weapon_manager.gd
@@ -8,12 +8,16 @@ var weapon_map = {
 	"sword": {
 		"scene": preload("uid://b0nre1wp17b5l"),
 	},
+	"BigSword": {
+		"scene": preload("uid://creq4ecbuhbfc")
+	},
 	"DispellingSword": {
 		"scene": preload("uid://bqgvi3j3xs1ku"),
 	},
 }
 
 func get_random_weapon_id()-> String:
+	return "BigSword"
 	return "DispellingSword"
 	return weapon_map.keys().pick_random()
 
