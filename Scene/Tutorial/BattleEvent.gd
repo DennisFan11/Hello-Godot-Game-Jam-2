3b2c2be36b2153506c1467b679a77b6a0db3b53d
DennisFan(Mac <dennis515jd@gmail.com>
2025-07-29 14:47:44 +0800

    Tutorial  Enemy fix

--- Scene/Tutorial/BattleEvent.gd
+++ Scene/Tutorial/BattleEvent.gd
@@ -4,11 +4,12 @@ extends DistanceSignal
 var _enemy_manager: EnemyManager
 func _inside():
 	#print("BATTLE EVENT")
-	for i in range(3):
+	for i in range(5):
 		_spawn_enemy()
 	
 
 func _spawn_enemy():
+	var rand_pos = Vector2( randfn(-10, 10), randfn(-10, 10))
 	_enemy_manager.spawn_enemy_force(
 		EnemyManager.ENEMY_TYPE.B,
-		global_position)
+		global_position + rand_pos)
