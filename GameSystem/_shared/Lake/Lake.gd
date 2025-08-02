5386995593c054a7207dc6b47934d4b682fbc319
rabbit99 <vvbest2012@gmail.com>
2025-08-02 23:21:30 +0800

    allows the lake to be entered only once

--- GameSystem/_shared/Lake/Lake.gd
+++ GameSystem/_shared/Lake/Lake.gd
@@ -1,6 +1,13 @@
 extends Interactable
 
+var _has_activated: bool = false
+
 func interact() -> void:
+	if _has_activated:
+		print("Already activated")
+		return
+	%RichTextLabel.text = "進入湖泊的通道消失了..."
+	_has_activated = true
 	var meetings_count = InGameSaveSystem.load_object("meetings_count")
 	if not meetings_count:
 		meetings_count = 0
