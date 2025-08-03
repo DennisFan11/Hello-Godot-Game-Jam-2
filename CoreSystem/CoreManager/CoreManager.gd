f3527c78200281aae09a8505669176b461f5ef47
rabbit99 <vvbest2012@gmail.com>
2025-08-03 14:40:32 +0800

    update level5

--- CoreSystem/CoreManager/CoreManager.gd
+++ CoreSystem/CoreManager/CoreManager.gd
@@ -24,6 +24,9 @@ var SCENE := {
 	"Level4": {
 		"file": preload("uid://cw45uprex267m")
 	},
+	"Level5": {
+		"file": preload("uid://deqbuv8vdfbuf")
+	},
 	"Level6": {
 		"file": preload("uid://jghh2wj4o126")
 	},
